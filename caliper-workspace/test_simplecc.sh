abed9953802aa85d59a98a7abf9801a159e5ddda
JackAugust <JackAugustZJ@gmail.com>
2022-12-21 21:34:00 +0800

    去除所有数据

--- caliper-workspace/test_simplecc.sh
+++
@@ -1,7 +0,0 @@
-npm init -y
-
-npm install --only=prod @hyperledger/caliper-cli@0.4.2
-
-npx caliper bind --caliper-bind-sut fabric:2.2
-
-npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/networkConfig.json --caliper-benchconfig benchmarks/simpleccBenchmark.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled --caliper-fabric-gateway-discovery
